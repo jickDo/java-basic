56c2f9c60a481fd4c79ddb713c5c9706e71db975
jickDo <pocard0117@naver.com>
2023-12-22 13:30:15 +0900

    같은 이름의 패키지는 임포트를 동시에 할수없다

---
+++ src/pack/PackageMain4.java
@@ -0,0 +1,12 @@
+package pack;
+
+import pack.a.User;
+import pack.a.User2;
+
+public class PackageMain4 {
+
+    public static void main(String[] args) {
+        User userA = new User();
+        pack.b.User userB = new pack.b.User();
+    }
+}
---
+++ src/pack/b/User.java
@@ -0,0 +1,8 @@
+package pack.b;
+
+public class User {
+
+    public User(){
+        System.out.println("패키지 pack.b가 생성되었습니다");
+    }
+}
