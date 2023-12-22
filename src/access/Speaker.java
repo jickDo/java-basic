92a3655c46709f81fc4f47672df705f3778baf72
jickDo <pocard0117@naver.com>
2023-12-22 13:52:10 +0900

    스피커 클래스 생성

---
+++ src/access/Speaker.java
@@ -0,0 +1,27 @@
+package access;
+
+public class Speaker {
+    int volume;
+
+    Speaker(int volume){
+        this.volume= volume;
+    }
+
+    void volumeUp(){
+        if (volume>=100){
+            System.out.println("최대 볼륨입니다");
+        }else{
+            volume+=10;
+            System.out.println("음량이 10 증가되었습니다");
+        }
+    }
+
+    void volumeDown(){
+        volume-=10;
+        System.out.println("음량이 10 감소되었습니다");
+    }
+
+    void showVolume(){
+        System.out.println("현재 음량은? "+volume);
+    }
+}
