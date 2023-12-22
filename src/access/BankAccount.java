1749e629a321d100ed0cc1cbecae15069e275ec7
jickDo <pocard0117@naver.com>
2023-12-22 15:14:19 +0900

    캡슐화를 고려한 은행 예제

---
+++ src/access/BankAccount.java
@@ -0,0 +1,38 @@
+package access;
+
+public class BankAccount {
+
+    private int balance;
+
+    public BankAccount() {
+        balance=0;
+    }
+
+    public void deposit(int amount){
+        if (isAmountValid(amount)){
+            balance+=amount
+        }else {
+            System.out.println("잘못된 입급입니다.");
+        }
+    }
+
+    public void withdraw(int amount){
+        if (isAmountValid(amount)&& balance>=amount){
+            balance-=amount;
+        }else {
+            System.out.println("잘못된 출금입니다.");
+        }
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+
+    private boolean isAmountValid(int amount){
+        return amount>0;
+    }
+
+
+
+}
