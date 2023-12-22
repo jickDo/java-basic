b9d3d3d5ec5def305b500fadfbdc64f57a361220
jickDo <pocard0117@naver.com>
2023-12-22 13:58:28 +0900

    문제발생: 멤버변수에 직접 접근이 가능해서 문제가됨

---
+++ src/access/SpeakerMain1.java
@@ -0,0 +1,23 @@
+package access;
+
+import java.nio.file.attribute.UserPrincipalLookupService;
+
+public class SpeakerMain1 {
+
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+
+        speaker.showVolume();
+        speaker.volumeUp();
+        speaker.volumeUp();
+        speaker.volumeDown();
+        speaker.volumeDown();
+        speaker.showVolume();
+
+
+        //필드에 직접 접근
+        System.out.println("필드에 직접 접근");
+        speaker.volume=200;
+        speaker.showVolume();
+    }
+}
