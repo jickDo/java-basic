03d829e78dcff4624a19aaf44a1f5584f94901e5
jickDo <pocard0117@naver.com>
2023-12-22 14:39:26 +0900

    다른 디렉토리에서 접근제어자 호출 예제

--- src/access/b/AccessOuterMain.java
+++ src/access/b/AccessOuterMain.java
@@ -1,2 +1,24 @@
-package access.b;public class AccessOuterMain {
+package access.b;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+
+    public static void main(String[] args) {
+        AccessData accessData = new AccessData();
+
+        //public 호출 가능
+        accessData.publicField=10;
+        accessData.publicMethod();
+
+        //default 패키지 불가능
+//        accessData.defaultField=5;
+//        accessData.defaultMethod();
+
+        //private 호출 불가능
+//        accessData.privateField =1;
+//        accessData.privateMethod();
+
+        accessData.innerAccess();
+    }
 }
