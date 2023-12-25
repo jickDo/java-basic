1a77162eec2738c69b25c3bb46714e49677fa714
jickDo <pocard0117@naver.com>
2023-12-25 18:46:24 +0900

    인스턴스와 스태틱 비교

---
+++ src/static2/DecoDataMain.java
@@ -0,0 +1,19 @@
+package static2;
+
+public class DecoDataMain {
+
+    public static void main(String[] args) {
+        System.out.println("1. 정적 호출");
+        DecoData.staticCall();
+
+        System.out.println("2. 인스턴스 호출");
+        DecoData decoData = new DecoData();
+        decoData.instanceCall();
+
+        System.out.println("3. 인스턴스 호출2");
+        DecoData decoData2 = new DecoData();
+        decoData2.instanceCall();
+
+
+    }
+}
