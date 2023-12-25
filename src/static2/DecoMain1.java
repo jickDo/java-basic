e3b3dd44172008b8c2175ec44040c42245084de9
jickDo <pocard0117@naver.com>
2023-12-25 18:11:33 +0900

    static 메서드의 필요성

---
+++ src/static2/DecoMain1.java
@@ -0,0 +1,13 @@
+package static2;
+
+public class DecoMain1 {
+
+    public static void main(String[] args) {
+        String s = "Hello Java";
+        DecoUtil1 decoUtil1 = new DecoUtil1();
+        String deco = decoUtil1.deco(s);
+
+        System.out.println("before: "+ s);
+        System.out.println("after: "+ deco);
+    }
+}
