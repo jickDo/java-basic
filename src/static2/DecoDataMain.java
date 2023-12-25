7c2f4d7ad34216009a1a3ba6a2d6423ddafdce21
jickDo <pocard0117@naver.com>
2023-12-25 18:52:20 +0900

    static 메서드를 함축하기 위해 import 할수있다.

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,16 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출");
         DecoData decoData = new DecoData();
