72c8bbd1bf8374aaf7148bcac1a3af7e8dc0cb0a
jickDo <pocard0117@naver.com>
2023-12-26 16:17:20 +0900

    final 멤버 변수로 사용시 생성자에서 한번 초기화 가능

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+    //final을 맴버 변수로 사용시 생성자에서 한번 초기화 가능
+}
