632a913f994061fdf0681422a9f12c54f41a6b11
jickDo <pocard0117@naver.com>
2023-12-26 20:45:53 +0900

    오버라이딩 예제

---
+++ src/extends1/overriding/CarMain.java
@@ -0,0 +1,21 @@
+package extends1.overriding;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.charge();
+        electricCar.move();
+        electricCar.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.fillUp();
+        gasCar.move();
+        gasCar.openDoor();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.move();
+        hydrogenCar.openDoor();
+    }
+}
