5a1805f4492e24846d7fd94001044a47645bdd02
jickDo <pocard0117@naver.com>
2023-12-26 16:23:03 +0900

    final을 여러 인스턴스로 생성할때는 상수를 권장

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value  = 10;
+
+//    public FieldInit(int value) {
+//        this.value= value;
+//    } 이미 선언된 final은 생성자 초기화 불가
+}
