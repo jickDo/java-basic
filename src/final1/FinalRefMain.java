50883f29332d7f7c2197053aee576ecdccaa8f45
jickDo <pocard0117@naver.com>
2023-12-26 16:38:27 +0900

    final 객체의 값은 변경가능

---
+++ src/final1/FinalRefMain.java
@@ -0,0 +1,14 @@
+package final1;
+
+public class FinalRefMain {
+    public static void main(String[] args) {
+        final Data data = new Data();
+
+        data.value=10;
+        data.value=20;
+        data.value=30;
+
+        //참조 대상의 값은 변경가능함
+        //final 객체 생성시 주의사항
+    }
+}
