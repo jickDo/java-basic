8eb0c81b0a53496ce40d1e0c8692814ec2af2d06
jickDo <pocard0117@naver.com>
2023-12-27 14:44:06 +0900

    부모 자식 생성자 생성

--- src/extends1/test/Album.java
+++ src/extends1/test/Album.java
@@ -3,4 +3,9 @@ package extends1.test;
 public class Album extends Item{
 
     private String artist;
+
+    public Album(String name, int price, String artist) {
+        super(name, price);
+        this.artist = artist;
+    }
 }
--- src/extends1/test/Book.java
+++ src/extends1/test/Book.java
@@ -5,4 +5,9 @@ public class Book extends Item{
     private String author;
     private String isbn;
 
+    public Book(String name,int price ,String author, String isbn) {
+        super(name,price);
+        this.author = author;
+        this.isbn = author;
+    }
 }
