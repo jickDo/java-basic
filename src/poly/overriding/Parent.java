2ae8f626f3d9df0228976112fd7134b5e4415cab
jickDo <pocard0117@naver.com>
2023-12-27 17:14:24 +0900

    다형성의 오버라이딩 예제

--- src/poly/overriding/Parent.java
+++ src/poly/overriding/Parent.java
@@ -2,7 +2,7 @@ package poly.overriding;
 
 public class Parent {
 
-    public String value;
+    public String value="parent";
 
     public void method(){
         System.out.println("Parent.method");
