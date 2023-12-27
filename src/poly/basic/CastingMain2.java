672e305f604457dc132b8fcd75a9731917cf32ab
jickDo <pocard0117@naver.com>
2023-12-27 15:56:40 +0900

    일시적 다운 캐스팅

---
+++ src/poly/basic/CastingMain2.java
@@ -0,0 +1,22 @@
+package poly.basic;
+
+public class CastingMain2 {
+
+    public static void main(String[] args) {
+        //부모 변수가 자식 인스턴스 참조 (다형적 참조)
+        Parent poly = new Child();
+        //단 자식의 기능은 호출할 수 없다.
+        //poly.childMethod();
+
+        //다운캐스팅( 부모타입 -> 자식타입)
+//        Child child = poly;
+//        자식은 부모를 담을수없다.
+        Child child = (Child) poly;
+        //강제로 캐스팅하면 된다.
+        child.childMethod();
+
+        //일시적 다운캐스팅 - 해당 메소드를 호출하는 순간만 다운 캐스팅
+        ((Child) poly).childMethod();
+
+    }
+}
