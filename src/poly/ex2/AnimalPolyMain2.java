d81c94e958afefa589aaf0b22384fde21edcd549
jickDo <pocard0117@naver.com>
2023-12-28 15:13:28 +0900

    다형성 예제 1을 향상된 for문으로 리팩토링

---
+++ src/poly/ex2/AnimalPolyMain2.java
@@ -0,0 +1,23 @@
+package poly.ex2;
+
+public class AnimalPolyMain2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Duck duck = new Duck();
+
+        Animal[] animals = {dog,cat,caw,duck};
+
+        for(Animal animal:animals){
+            System.out.println("동물 소리 테스트 시작");
+            animal.sound();
+            System.out.println("동물 소리 테스트 \n");
+        }
+
+        //부모는 자식을 담을수 있다 만세 삼창!!!
+        //여러개의 타입을 다형적 참조로 Animal로 통일
+        //메서드 오버라이딩으로 실행시점에 각각의 메서드 출력
+    }
+}
