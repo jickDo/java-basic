0a7fd287771b9ce73163805191449a379755f108
jickDo <pocard0117@naver.com>
2023-12-28 15:05:10 +0900

    다형성 1.다형적 참조 2. 메서드 오버라이딩 첫 활용

--- src/poly/ex1/AnimalSoundMain1.java
+++ src/poly/ex1/AnimalSoundMain1.java
@@ -12,5 +12,9 @@ public class AnimalSoundMain1 {
         caw.sound();
         cat.sound();
     }
-
+    private static void sound(Caw caw){
+        System.out.println("동물 소리 테스트 시작");
+        caw.sound();
+        System.out.println("동물 소리 테스트 끝");
+    }
 }
---
+++ src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,20 @@
+package poly.ex2;
+
+public class AnimalPolyMain1 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw); //부모는 자식을 담을수 있다 만세 삼창!!!
+    }
+
+    private static void soundAnimal(Animal animal){
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 \n");
+    }
+}
