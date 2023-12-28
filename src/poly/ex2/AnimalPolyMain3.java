4e0d3e9edd07a6d765c5220b0de5c55e73c55ef3
jickDo <pocard0117@naver.com>
2023-12-28 15:15:53 +0900

    다형성 예제 활용 리팩토링 2

---
+++ src/poly/ex2/AnimalPolyMain3.java
@@ -0,0 +1,23 @@
+package poly.ex2;
+
+public class AnimalPolyMain3 {
+
+    public static void main(String[] args) {
+
+        Animal[] animals = {new Dog(), new Cat(), new Caw(), new Duck()};
+
+        for(Animal animal:animals){
+            soundAnimal(animal);
+        }
+
+        //부모는 자식을 담을수 있다 만세 삼창!!!
+        //여러개의 타입을 다형적 참조로 Animal로 통일
+        //메서드 오버라이딩으로 실행시점에 각각의 메서드 출력
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 \n");
+    }
+}
