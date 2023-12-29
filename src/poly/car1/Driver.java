7516ffa6c0155cd8defee8efccf9aea67bf2a9e9
jickDo <pocard0117@naver.com>
2023-12-30 00:43:56 +0900

    다형적으로 driver 리팩토링

---
+++ src/poly/car1/Driver.java
@@ -0,0 +1,20 @@
+package poly.car1;
+
+public class Driver {
+
+    private Car car;
+
+    public Driver(Car car) {
+        System.out.println("자동차를 설정합니다");
+        this.car = car;
+    }
+
+    public void drive(){
+        System.out.println("자동차를 운전합니다");
+        car.startEngine();
+        car.pressAccelerator();
+        car.stopEngine();
+    }
+
+
+}
