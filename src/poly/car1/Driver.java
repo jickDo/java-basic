432ca98d2837772e830fe4c425fbed82318d9f7a
jickDo <pocard0117@naver.com>
2023-12-30 18:17:13 +0900

    새로운 차를 추가하며 ocp패턴 익히기

--- src/poly/car1/Driver.java
+++ src/poly/car1/Driver.java
@@ -4,8 +4,8 @@ public class Driver {
 
     private Car car;
 
-    public Driver(Car car) {
-        System.out.println("자동차를 설정합니다");
+    public void setCar(Car car) {
+        System.out.println("자동차를 선택합니다 "+car);
         this.car = car;
     }
 
