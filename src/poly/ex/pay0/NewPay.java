ab5ede1c6cc8b7526c637c72d7e18bcf86a9f92c
jickDo <pocard0117@naver.com>
2023-12-30 19:15:10 +0900

    새로운 시스템이 추가될때 상황

---
+++ src/poly/ex/pay0/NewPay.java
@@ -0,0 +1,26 @@
+package poly.ex.pay0;
+
+public class NewPay implements PayPlatform{
+    private int money=0;
+
+    @Override
+    public void pay() {
+        System.out.println(money+"원 결제 합니다");
+        validAmount(money);
+    }
+
+    @Override
+    public void deposit(int amount) {
+        System.out.println("newPay를 충전합니다 "+amount);
+        this.money+=amount;
+    }
+
+    @Override
+    public void validAmount(int amount) {
+        if (money>=amount){
+            System.out.println("결제에 성공합니다");
+        } else {
+            System.out.println("잔액이 부족합니다");
+        }
+    }
+}
--- src/poly/ex/pay0/PayMain0.java
+++ src/poly/ex/pay0/PayMain0.java
@@ -5,14 +5,19 @@ public class PayMain0 {
             PayService payService = new PayService();
 
             //kakao 결제
-            String payOption1 = "kakao";
+            KakaoPay kakaoPay = new KakaoPay();
             int amount1 = 5000;
-            payService.processPay(payOption1, amount1);
+            payService.processPay(kakaoPay, amount1);
 
             //naver 결제
-            String payOption2 = "naver";
+            NaverPay naverPay = new NaverPay();
             int amount2 = 10000;
-            payService.processPay(payOption2, amount2);
+            payService.processPay(naverPay, amount2);
+
+            //새로운 결제수단 추가
+            NewPay newPay = new NewPay();
+            int amount4 = 20000;
+            payService.processPay(newPay,amount4);
 
             //잘못된 결제 수단 선택
             String payOption3 = "bad";
